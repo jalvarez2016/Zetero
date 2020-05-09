79329b85d18bf9bb3c1cc90c0b9ae79cedbc1612
Jason <jasonalvarezcoraiza@gmail.com>
2020-05-09 12:17:07 -0400

    Added a bunch of assets
    Crouch animatino has been configured. Background bricks have been added and idle animation has been tweaked. Jumping force may need a little work though

--- Assets/PlayerMovementScript.cs
+++ Assets/PlayerMovementScript.cs
@@ -8,7 +8,7 @@ public class PlayerMovementScript : MonoBehaviour
     public Vector3 leftMovement;
     public Vector3 rightMovement;
     public Vector3 jumpForce;
-    private Vector3 balconySpawn;
+    public GameObject person;
     public Animator player;
 
     void OnCollisionEnter2D(Collision2D other)
@@ -29,34 +29,47 @@ public class PlayerMovementScript : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {        
+        // Debug.Log("collided and hasJumped is " + hasJumped);
+        Vector2 velocity = person.GetComponent<Rigidbody2D>().velocity;
+        Debug.Log("player y velocity is " + velocity.y);
+
+        //left and right movement
         if(Input.GetAxisRaw("Horizontal") == -1 &&  player.GetFloat("Speed") >= -player.GetInteger("MaxSpeed")){
+            // Debug.Log(person.GetComponent<Rigidbody2D>().velocity);
             GetComponent<Rigidbody2D>().AddForce(leftMovement);
             player.SetBool("right",true);
             GameObject.Find("Player").GetComponent<SpriteRenderer>().flipX = true;
             player.SetBool("idle", false);
-            // acceleration = (rigidbody.velocity - lastVelocity) / Time.fixedDeltaTime;
-            // lastVelocity = rigidbody.velocity;
-            var newSpeed = player.GetFloat("Speed") + leftMovement.x;
+            var newSpeed = velocity.x;
             player.SetFloat("Speed", newSpeed)  ;
         } else if(Input.GetAxisRaw("Horizontal") == 1 && player.GetFloat("Speed") <= player.GetInteger("MaxSpeed")){
+            // Debug.Log(person.GetComponent<Rigidbody2D>().velocity);
             GetComponent<Rigidbody2D>().AddForce(rightMovement);
             player.SetBool("right", true);
             GameObject.Find("Player").GetComponent<SpriteRenderer>().flipX = false;
             player.SetBool("idle", false);
-            var newSpeed = player.GetFloat("Speed") + rightMovement.x;
+            var newSpeed = velocity.x;
             player.SetFloat("Speed", newSpeed);
         } else {
             player.SetBool("right", false);
             player.SetBool("idle", true);
         }
+
+        //jumping and crouching 
         if(Input.GetAxisRaw("Vertical") == 1 && !hasJumped){
             hasJumped = true;
             GetComponent<Rigidbody2D>().AddForce(jumpForce);
             player.SetBool("up", true);
         } else if(Input.GetAxisRaw("Vertical") == -1){
             player.SetBool("down", true);
+        } else if(Input.GetAxisRaw("Vertical") != -1){
+            player.SetBool("down", false);
+        }
+
+        //correcting jumping error
+        if(velocity.y == 0){
+            hasJumped = false;
         }
         
     }
